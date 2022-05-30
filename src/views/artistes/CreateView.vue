3ea1411f4a46ad60365ed8eedcb40833ba3ade28
Lilian <94964246+LilianChesneau@users.noreply.github.com>
2022-05-30 15:57:30 +0200

    Ajouter un artiste visuel

--- src/views/artistes/CreateView.vue
+++ src/views/artistes/CreateView.vue
@@ -1,196 +1,145 @@
 <template>
-    <div class="container md:pt-[200px]">
-        <form enctype="multipart/form-data" 
-            @submit.prevent="createArtistes"
-        >
-            <div class="card bg-dark">
-
-                <div class="card-header">
-                    <h5 style="color:white;">Ajouter un artiste au festival</h5>
-                </div>    
-
-                <div class="card-body">   
-                    <div class="row">
-                        <div class="col-6">
-                            <div>
-                                <img class="preview img-fluid" :src="imageData"/>
-                            </div>
-                        </div>
-
-                        <div class="col-6">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" >Nom</span>
-                                </div>
-                                <input 
-                                    class="form-control" placeholder="Nom de la personne"
-                                    v-model="artistes.nom"
-                                    required />                    
-                            </div>
-                            <br/>
-                            <br/>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">Photo</span>
-                                </div>
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" ref="file" id="file" 
-                                    @change="previewImage"
-                                    >
-                                    <label class="custom-file-label" for="file">Sélectionner l'image</label>
-                                </div>
-                            </div>
-                            <br/>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" >Date naissance</span>
-                                </div>
-                                <input 
-                                    type="date"
-                                    class="form-control"
-                                    v-model="artistes.naissance" 
-                                    format="dd/mm/yyyy" 
-                                    required />                    
-                            </div>
-                            <br/>
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text" >Pays</span>
-                                </div>
-                                <select class="custom-select" v-model="artistes.nationalite">
-                                    <option selected disabled>Sélectionner un Pays</option>
-                                    <option v-for="pays in listePays" :key="pays.nom">
-                                        {{pays.nom}}
-                                    </option>
-                                </select>
-                            </div>
-                            <br/>
-                        </div>
-                    </div>               
-                </div>
-
-                <div class="card-footer">   
-                    <button type="submit" class="float-left btn btn-dark">
-                        Créer
-                    </button>
-                    <button class="float-right btn btn-dark" >
-                        <router-link to="/artistes" >Cancel</router-link>
-                    </button>
-                </div>
-
-            </div>
-        </form>        
+<div class="md:pt-[90px]">
+  <div class="md:flex md:max-h-[830px]">
+    <div class="m-auto">
+      <img class="m-auto max-h-[500px] md:flex-auto" src="../../../public/logosx3.jpg" alt="Illustration Deep Down">
+      <div class="my-[10%]">
+        <div class="mt-[5%] mb-2 flex justify-center">
+          <h1 class="font-210 text-5xl text-center">AJOUTER UN ARTISTE</h1>
+        </div>
+        <underline/>
+      </div>
+      <img class="m-auto max-h-[500px] md:flex-auto" src="../../../public/logosx3.jpg" alt="Illustration Deep Down">
     </div>
+    <div class="md:m-auto">
+      <img class="md:max-h-[1000px]" src="../../../public/Logosdecor.jpg" alt="Logos du Deep Down Festival">
+    </div>
+  </div>
+
+  <section class="mt-[5%]">
+    <h1 class="font-210-box italic text-5xl text-center mb-5">Ajouter un artiste au festival</h1>
+    <div class="xl:flex">
+      <div class="md:w-[50%] border-2 border-accent rounded-3xl p-2 xl:mb-0 mx-auto mb-[5%]">
+        <img :src="imageData" class="rounded-3xl"/>
+      </div>
+      <div class="xl:pl-3 xl:my-auto">
+        <form @submit.prevent="createArtistes">
+          <div>
+            <div>
+                <div class="flex items-center gap-2 mb-3">
+                    <div class="bg-white text-sombre p-2 rounded-xl w-[200px] xl:w-[250px] text-center">
+                        <span class="font-cairo font-black text-lg" >Nom</span>
+                    </div>
+                      <input class="flex-auto bg-white text-sombre py-2 px-3 rounded-lg" placeholder="Complétez avec un nom" v-model="artistes.nom" required />                    
+                  </div>
+              </div>
+              <div>
+                <div class="flex items-center gap-2 mb-3">
+                    <div class="bg-white text-sombre p-2 rounded-xl w-[200px] xl:w-[250px] text-center">
+                        <span class="font-cairo font-black text-lg" >Image</span>
+                    </div>
+                      <input class="flex-auto bg-white text-sombre py-2 px-3 rounded-lg" type="file" placeholder="Ajouter un fichier" ref="file" id="file" @change="previewImage">               
+                  </div>
+              </div>
+              <div>
+                <div class="flex items-center gap-2 mb-3">
+                    <div class="bg-white text-sombre p-2 rounded-xl w-[200px] xl:w-[250px] text-center">
+                        <span class="font-cairo font-black text-lg" >Date de naissance</span>
+                    </div>
+                    <input type="date" class="flex-auto bg-white text-sombre py-2 px-3 rounded-lg" v-model="artistes.naissance" format="dd/mm/yyyy" required />                    
+                </div>
+              </div>
+              <div>
+                <div class="flex items-center gap-2 mb-3">
+                    <div class="bg-white text-sombre p-2 rounded-xl w-[200px] xl:w-[250px] text-center">
+                        <span class="font-cairo font-black text-lg" >Pays</span>
+                    </div>
+                    <select class="flex-auto bg-white text-sombre py-2 px-3 rounded-lg" v-model="artistes.nationalite">
+                      <option selected disabled>Sélectionner un Pays</option>
+                      <option v-for="pays in listePays" :key="pays.nom">{{pays.nom}}</option>
+                    </select>                    
+                </div>
+              </div>
+            </div>
+            <div class="flex justify-evenly mt-[5%]">
+              <button class="bg-sombre text-white border-2 border-accent font-210 px-3 py-2 rounded-3xl">
+                <router-link to="/artistes">Retour</router-link>
+              </button>
+              <button type="submit" class="bg-accent text-white border-2 border-white font-210 px-3 py-2 rounded-3xl">Creer</button>
+            </div>
+          </form>
+      </div>
+    </div>
+  </section>
+
+  <DDbas class="mt-[100px]"/>
+
+</div>
 </template>
 
 <script>
-// Bibliothèque Firestore : import des fonctions
-import { 
-    getFirestore,   // Obtenir le Firestore
-    collection,     // Utiliser une collection de documents
-    doc,            // Obtenir un document par son id
-    getDocs,        // Obtenir la liste des documents d'une collection
-    addDoc,         // Ajouter un document à une collection
-    updateDoc,      // Mettre à jour un document dans une collection
-    deleteDoc,      // Supprimer un document d'une collection
-    onSnapshot,     // Demander une liste de documents d'une collection, en les synchronisant
-    query,          // Permet d'effectuer des requêtes sur Firestore
-    orderBy         // Permet de demander le tri d'une requête query
-    } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js'
+import underline from "../../../src/components/decors/UnderlineView.vue"
+import DDbas from "../../../src/components/FooterView.vue"
 
-
-    // Cloud Storage : import des fonctions
-    import { 
-        getStorage,             // Obtenir le Cloud Storage
-        ref,                    // Pour créer une référence à un fichier à uploader
-        getDownloadURL,         // Permet de récupérer l'adress complète d'un fichier du Storage
-        uploadString,           // Permet d'uploader sur le Cloud Storage une image en Base64
-    } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js'
+import { getFirestore, collection, doc, getDocs, addDoc, updateDoc, deleteDoc, onSnapshot, query, orderBy } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js'
+import { getStorage, ref, getDownloadURL, uploadString } from 'https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js'
 
  
 export default {
     name:'CreateView',
+    components:{ underline, DDbas},
     data() {
         return {
-            imageData:null,         // Image prévisualisée
-            listePays:[],           // Liste des pays pour la nationalité du participant
-            artistes:{           // Le participant à créer
-                nom:null,               // son nom
-                photo:null,             // sa photo (nom du fichier)
-                naissance:null,         // sa date de naissance
-                nationalite:null        // sa nationalité
+            imageData:null,
+            listePays:[],
+            artistes:{
+                nom:null,
+                photo:null,
+                naissance:null,
+                nationalite:null
             }
         }
     },
-    mounted(){ // Montage de la vue
-        // Appel de la liste des pays
+    mounted(){
         this.getPays();
     },
     methods : {
-        async getPays(){            
-            // Obtenir Firestore
+        async getPays(){
             const firestore = getFirestore();
-            // Base de données (collection)  document pays
-            const dbPays = collection(firestore, "pays");
-            // Liste des participants triés
-            // query permet de faire une requête sur Firebase
-            // notement pour filtrer, trier ... des données
-            //orderBy permet de préciser sur quel élément trier, et dans quel ordre
-            // ici le nom du pays par ordre croissant (asc)            
+            const dbPays = collection(firestore, "pays");          
             const q = query(dbPays, orderBy('nom', 'asc'));
-            // Récupération de la liste des pays à partir de la query
-            // La liste est synchronisée
             await onSnapshot(q, (snapshot) => {
                 this.listePays = snapshot.docs.map(doc => (
                     {id:doc.id, ...doc.data()}
                 ))  
-console.log("Liste des pays", this.listePays);      
+            console.log("Liste des pays", this.listePays);      
             })      
         },
 
         previewImage: function(event) {
-            // Mise à jour de la photo du participant
             this.file = this.$refs.file.files[0];
-            // Récupérer le nom du fichier pour la photo du participant
             this.artistes.photo = this.file.name;
-            // Reference to the DOM input element
-            // Reference du fichier à prévisualiser
             var input = event.target;
-            // On s'assure que l'on a au moins un fichier à lire
             if (input.files && input.files[0]) {
-                // Creation d'un filereader
-                // Pour lire l'image et la convertir en base 64
                 var reader = new FileReader();
-                // fonction callback appellée lors que le fichier a été chargé
                 reader.onload = (e) => {
-                    // Read image as base64 and set to imageData
-                    // lecture du fichier pour mettre à jour 
-                    // la prévisualisation
                     this.imageData = e.target.result;
                 }
-                // Demarrage du reader pour la transformer en data URL (format base 64) 
                 reader.readAsDataURL(input.files[0]);        
             }
         },
 
         async createArtistes(){
-            // Obtenir storage Firebase
             const storage = getStorage();
-            // Référence de l'image à uploader
             const refStorage = ref(storage, 'artistes/'+this.artistes.photo);
-            // Upload de l'image sur le Cloud Storage
             await uploadString(refStorage, this.imageData, 'data_url').then((snapshot) => {
                 console.log('Uploaded a base64 string');
-                
-                // Création du participant sur le Firestore
                 const db = getFirestore();
                 const docRef = addDoc(collection(db, 'artistes'), this.artistes );
             });
-            // redirection sur la liste des participants
             this.$router.push('/artistes');            
         }
     }
-
 }
 </script>
 
